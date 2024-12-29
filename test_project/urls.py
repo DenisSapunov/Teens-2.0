4430a92a2745ecaf3172758e6a63c8cbcec53987
denis <denis.sapunov@rigelstorm.com>
2024-12-29 17:23:44 +0300

    test

--- test_project/urls.py
+++ test_project/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from app.views import index, product_detail, category_product, categories_list
+from app.views import index, product_detail, category_product, categories_list, search
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('product/<int:product_id>/', product_detail, name='product_detail'),
     path('category/<int:category_id>/', category_product, name='category_detail'),
     path('categories_list/', categories_list, name='categories_list'),
+    path('search/', search, name='search'),
 ]
