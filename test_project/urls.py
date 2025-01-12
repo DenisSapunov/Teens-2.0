d815c2b0485770d641c02789a1553dfe9648ed90
denis <denis.sapunov@rigelstorm.com>
2025-01-12 16:51:52 +0300

    auth

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from app.views import index, product_detail, category_product, categories_list
 
@@ -10,4 +10,8 @@ urlpatterns = [
     path('product/<int:product_id>/', product_detail, name='product_detail'),
     path('category/<int:category_id>/', category_product, name='category_detail'),
     path('categories_list/', categories_list, name='categories_list'),
+]
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
